c0279b2771c0aca9e99b8af8221f4e74750f178c
Meanderthal00 <meanderthal00@yahoo.com>
2018-01-31 10:14:17 -0500

    html work

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -13,6 +13,6 @@ module.exports = function (app) {
     });
 
     app.get("*", function (req,res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"))
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
