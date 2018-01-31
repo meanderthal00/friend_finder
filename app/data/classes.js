6eca276a55e2cd90b4d4620bca36cf38a915574e
Meanderthal00 <meanderthal00@yahoo.com>
2018-01-31 08:53:49 -0500

    rework

---
+++ app/data/classes.js
@@ -0,0 +1,64 @@
+var classArray = [
+    {
+      name: "Barbarian",
+      image: "../assets/images/iNeedToFindImages.jpg",
+      message:"It's all about causing the most damage as quickly as possible, you don't even bother with armor.",
+      scores:[
+           1,
+           2,
+           3,
+           4,
+           5,
+           6,
+           7,
+           8,
+           9,
+           0 
+
+      ]
+    },
+    {
+    name:"",
+    image:"",
+    message:"",
+    scores:[
+
+    ]
+},
+{
+    name:"",
+    image:"",
+    message:"",
+    scores:[
+
+    ]
+},
+{
+    name:"",
+    image:"",
+    message:"",
+    scores:[
+
+    ]
+},
+{
+    name:"",
+    image:"",
+    message:"",
+    scores:[
+
+    ]
+},
+{
+    name:"",
+    image:"",
+    message:"",
+    scores:[
+
+    ]
+},
+  ];
+
+
+  module.exports = classArray
+  
