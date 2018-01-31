a7f844e30f521fded6a6a640f0343337c9539b85
Meanderthal00 <meanderthal00@yahoo.com>
2018-01-31 08:48:00 -0500

    re push everything

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,17 @@
+// loading data from resources
+ var classData = require("..data/classes");
+
+//  ROUTING
+
+module.exports = function (app) {
+    // this section handles the GET requests
+
+    app.get("/api/classes", function(req, res) {
+        res.json(classArray);
+        console.log(classArray);
+
+    });
+    app.post("/api/classes", function(req,res){
+        console.log("routing test post, apiRoutes.js");
+    })
+}
