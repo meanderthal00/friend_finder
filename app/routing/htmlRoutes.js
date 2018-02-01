dd01ea02464a6fb8e04da6d267ce58300b91dffc
Meanderthal00 <meanderthal00@yahoo.com>
2018-02-01 18:26:43 -0500

    file structure

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,10 @@ var path = require("path");
 module.exports = function (app) {
     // get requests:
 
-   
+    app.get("/", function (req,res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
 
     app.get("/survey", function (req,res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
