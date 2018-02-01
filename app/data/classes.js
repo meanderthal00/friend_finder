32ce2154205ca2cf1af39a0c00fb911603f42acf
Meanderthal00 <meanderthal00@yahoo.com>
2018-02-01 07:05:28 -0500

    images added, classes scored, onclick in progress

--- app/data/classes.js
+++ app/data/classes.js
@@ -1,62 +1,94 @@
 var classArray = [
     {
       name: "Barbarian",
-      image: "../assets/images/iNeedToFindImages.jpg",
+      image: "../assets/images/barbarian.jpg",
       message:"It's all about causing the most damage as quickly as possible, you don't even bother with armor.",
       scores:[
-           1,
-           2,
+           5,
            3,
            4,
-           5,
-           6,
-           7,
-           8,
-           9,
-           0 
+           1,
+           2,
+           1,
+           3,
+           1,
+           1,
+           1 
 
       ]
     },
     {
-    name:"",
-    image:"",
-    message:"",
+    name:"Cleric",
+    image:"../assets/images/cleric.jpg",
+    message:"Using your divine connection you heal your allies and decimate your foes. ",
     scores:[
+        3,
+        2,
+        3,
+        3,
+        4,
+        1,
+        1,
+        3,
+        4,
+        1 
 
     ]
 },
 {
-    name:"",
-    image:"",
-    message:"",
+    name:"Paladin",
+    image:"../assets/images/paladin1.jpg",
+    message:"A knight in shining armor, protecting your allies and punishing your enemies.",
     scores:[
+        5,
+        2,
+        4,
+        1,
+        3,
+        4,
+        1,
+        4,
+        1,
+        1 
 
     ]
 },
 {
-    name:"",
-    image:"",
-    message:"",
+    name:"Rogue",
+    image:"../assets/images/rogue.jpg",
+    message:"Shadows and silence are  the tools of your trade.",
     scores:[
+        2,
+        5,
+        3,
+        3,
+        4,
+        1,
+        1,
+        2,
+        1,
+        5 
 
     ]
 },
 {
-    name:"",
-    image:"",
-    message:"",
+    name:"Wizard",
+    image:"../assets/images/wizard.jpg",
+    message:"Spellcaster Supreme you know the secrets of the universe, and how to turn them against your foes.",
     scores:[
+        1,
+        2,
+        1,
+        5,
+        3,
+        1,
+        3,
+        1,
+        5,
+        1 
 
     ]
-},
-{
-    name:"",
-    image:"",
-    message:"",
-    scores:[
-
-    ]
-},
+}
   ];
 
 
